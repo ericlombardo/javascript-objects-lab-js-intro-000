4a18e53adab4e58793820ba11aa8b4f0d5153aa5
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-08-13 17:29:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function unpdateObjectWithKeyAndValue(object, key, value) {
-  obj[key] = value;
-  return obj;
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
 }
