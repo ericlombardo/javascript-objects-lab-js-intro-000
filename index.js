2befe306d38659a995f166e9bcdcefaf1c413053
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-08-13 17:50:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 function deleteFromObjectByKey(object, key) {
   var newObject = Object.assign({}, object);
-  delete object.key
-  return object;
+  delete newObject.key
+  return newObject;
 }
